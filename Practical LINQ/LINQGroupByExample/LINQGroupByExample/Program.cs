c9c681c4798da29cfd26ae597b1ae4f40cc56070
Aaron Clarke <aarondclarke1@googlemail.com>
2018-02-22 21:52:25 +0000

    Finished PracticalLINQ

--- Practical LINQ/LINQGroupByExample/LINQGroupByExample/Program.cs
+++ Practical LINQ/LINQGroupByExample/LINQGroupByExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,33 @@ namespace LINQGroupByExample
         static void Main( string[] args )
         {
             BankAccounts theAccounts = new BankAccounts();
+
+            SumAges( theAccounts.m_BankAccounts );
+            Console.WriteLine("");
+            GetAgesInGroups( theAccounts.m_BankAccounts );
+
+            Console.ReadKey();
+        }
+
+        private static void SumAges( List<BankAccount> theAccounts )
+        {
+            var query = theAccounts.Sum( x => x.Age );
+
+            Console.WriteLine(query);
+        }
+
+        private static void GetAgesInGroups( List<BankAccount> theAccounts )
+        {
+            var query = theAccounts.GroupBy( x => x.Age, x => x.Age, ( groupKey, AgeTotal ) => new
+            {
+                Key = groupKey,
+                TotalAge = AgeTotal.Sum()
+            } );
+
+            foreach ( var x in query )
+            {
+                Console.WriteLine( $"Age of group: {x.Key}, Sum of Ages: {x.TotalAge}" );
+            }
         }
     }
 }
---
+++ Practical LINQ/LINQMeanMedianModeExample/LINQMeanMedianModeExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQGroupByExample;
+
+namespace LINQMeanMedianModeExample
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            BankAccounts theAccounts = new BankAccounts();
+
+            CalculateMean( theAccounts.m_BankAccounts );
+            Console.WriteLine();
+            CalculateMedian( theAccounts.m_BankAccounts );
+            Console.WriteLine();
+            CalculateMode( theAccounts.m_BankAccounts );
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        private static void CalculateMean( List<BankAccount> theAccounts )
+        {
+            Console.WriteLine(theAccounts.Average( x => x.Age  ));
+        }
+
+        private static void CalculateMedian( List<BankAccount> theAccounts )
+        {
+            theAccounts.OrderBy( x => x.Age );
+            int MiddlePos = theAccounts.Count / 2;
+            decimal median;
+
+            if ( theAccounts.Count % 2 == 0 )
+            {
+                median = ( theAccounts[ MiddlePos - 1 ].Age + theAccounts[ MiddlePos + 1 ].Age ) / 2;
+            }
+            else
+            {
+                median = theAccounts[ MiddlePos ].Age;
+            }
+            Console.WriteLine(median);
+        }
+
+        private static void CalculateMode( List<BankAccount> theAccounts )
+        {
+            var query = theAccounts.GroupBy( x => x.Age ).OrderBy( x => x.Count() );
+            Console.WriteLine(query.LastOrDefault().Key);
+        }
+    }
+}
